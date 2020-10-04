5cbc58d8905f51a5f00a7e4b5347d59c5b1fb042
staceyk1234 <71597947+staceyk1234@users.noreply.github.com>
2020-10-04 16:17:05 +0100

    Override bug to replace forecast when new city added

--- src/script.js
+++ src/script.js
@@ -79,6 +79,7 @@ function displayWeatherCondition(response) {
 // F
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
   let forecast = null;
 
   for (let index = 0; index < 6; index++) {
@@ -106,7 +107,6 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 
-  // F
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
