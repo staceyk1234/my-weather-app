ef195a5b92f78e245d472caee0b691e8ac68b06b
staceyk1234 <71597947+staceyk1234@users.noreply.github.com>
2020-10-04 13:31:45 +0100

    Adding main icon

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 // Change current date and time when reloaded
-function formatDate(date) {
+function formatDate() {
   let now = new Date();
 
   let currentDayTime = document.querySelector("#current-day-time");
@@ -50,6 +50,7 @@ formatDate();
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#current-city");
+  let iconElement = document.querySelector("#icon");
   document.querySelector("#current-city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
 
@@ -61,12 +62,52 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+// F
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[0];
+  console.log(forecast);
+
+  forecastElement.innerHTML = `
+    <div class="col-2">
+      <p>
+        <strong>21:00</strong>
+      </p>
+      <svg
+        width="2.5em"
+        height="2.5em"
+        viewBox="0 0 16 16"
+        class="bi bi-cloud"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"
+        />
+      </svg>
+      <div class="forecast-temperature">
+        <strong>${Math.round(forecast.main.temp_max)}°</strong>
+      </div>
+    </div>
+`;
 }
 
 function searchCity(city) {
   let apiKey = "35a62bf353aac096a13a1e19a79bac44";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  // F
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
